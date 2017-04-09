d7cfbd614bf7614dde0c1788f75cc55babd74251
SamBusser30 <sabu0818@colorado.edu>
2017-04-09 17:07:09 -0600

    Update login.php

--- login.php
+++ login.php
@@ -1,17 +1,26 @@
+
 <?php
   session_start(); 
   if ($_POST['login']) {
     include_once("connect_to_db.php");
-    $username = strip_tags(($_POST['username']);    //removing tags in strings, making sure there is no SQL injection
-    $password = strip_tags(($_POST['password']);    //prevention method
+    $username = strip_tags($_POST['username']);    //removing tags in strings, making sure there is no SQL injection
+    $password = strip_tags($_POST['password']);    //prevention method
+
+    $username = stripslashes($_POST['username']);
+    $password = stripslashes($_POST['password']);
+
+    $username = mysqli_real_escape_string($username);
+    $password = mysqli_real_escape_string($password);
     
-    $username = stripslashes($username);
-    $password = stripslashes($password);
+    $password = md5($password); //encrypting password
 
-    $username = mysqli_real_escape_string($username);
-    $username = mysqli_real_escape_string($username);
+    $sql = "SELECT * FROM users WHERE username= '$username' LIMIT 1";  
+
+  }
+  
+
+
 ?>
-
 
 <html>
 <head>
